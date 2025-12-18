2145c9c1c0c2e414fbade98ff9aa0fc5b6628296
Roberto-Vender <185998296+Roberto-Vender@users.noreply.github.com>
2025-12-18 18:24:44 +0800

    Fix CORS for frontend domain

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,22 @@
 <?php
 
 return [
-	'paths' => ['api/*', 'sanctum/csrf-cookie'],
-	'allowed_methods' => ['*'],
-	'allowed_origins' => [
-		'http://localhost:5173',
-		'http://localhost:3000',
-		'https://application-dev-1-oat5.onrender.com',
-		'https://application-dev-1.onrender.com',
-	],
-	'allowed_origins_patterns' => [],
-	'allowed_headers' => ['*'],
-	'exposed_headers' => [],
-	'max_age' => 86400,
-	'supports_credentials' => true,
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://application-dev-1-1.onrender.com',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
 ];
