5c7e5bb6c5d99c0d6adf1ee40202d1e5881ea456
Roberto-Vender <185998296+Roberto-Vender@users.noreply.github.com>
2025-12-18 19:10:52 +0800

    dasd

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,10 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://application-dev-1-1.onrender.com',
+        'http://localhost:5173', // local dev React
+        'http://localhost:3000', // alternative local dev
+        'https://application-dev-1-1.onrender.com', // your deployed frontend
+        'https://application-dev-1-oat5.onrender.com', // optional previous frontend
     ],
 
     'allowed_origins_patterns' => [],
@@ -18,5 +21,5 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
